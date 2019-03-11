a081ac8ebedc4c2623efe76df1f8ace4a73715c1
AbreuRodrigo <rodrigo.abreu.ca@gmail.com>
2019-03-11 09:46:24 -0700

    Removed unecessary files

--- Assets/Infinity Code/Online maps/Scripts/Drawing/Managers/OnlineMapsDrawingElementManager.cs
+++
@@ -1,7 +0,0 @@
-﻿/*     INFINITY CODE 2013-2019      */
-/*   http://www.infinity-code.com   */
-
-public class OnlineMapsDrawingElementManager: OnlineMapsInteractiveElementManager<OnlineMapsDrawingElementManager, OnlineMapsDrawingElement>
-{
-    
-}
--- Assets/Infinity Code/Online maps/Scripts/Editor/Elevations/OnlineMapsArcGISElevationManagerEditor.cs
+++
@@ -1,9 +0,0 @@
-﻿/*     INFINITY CODE 2013-2019      */
-/*   http://www.infinity-code.com   */
-
-using UnityEditor;
-
-[CustomEditor(typeof(OnlineMapsArcGISElevationManager), true)]
-public class OnlineMapsArcGISElevationManagerEditor : OnlineMapsElevationManagerBaseEditor
-{
-}
